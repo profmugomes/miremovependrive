6380481996a5969f3258a450e66bdd4c8377aaa9
profmugomes <murilogomes5792@gmail.com>
2026-08-09 23:09:55 -0300

    Atualizado informacoes da licenca e pequenos ajustes

--- about.go
+++ about.go
@@ -1,11 +1,12 @@
-// Copyright (C) 2024-2026 Murilo Gomes Julio
-// SPDX-License-Identifier: GPL-2.0-only
+// Copyright (c) 2024-2026 Murilo Gomes Julio. All Rights Reserved.
 
-// Site: https://mugomes.github.io
+// Licensed under the PolyForm Strict License 1.0.0.
+// See LICENSE.md for details.
 
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"net/url"
 
@@ -13,22 +14,23 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
-	"github.com/mugomes/mgsmartflow"
+	"github.com/profmugomes/mgsmartflow/v2"
 )
 
-func showAbout(a fyne.App) {
+func showAbout() {
 	w := a.NewWindow("Sobre")
-	w.Resize(fyne.NewSize(597, 470))
+	w.Resize(fyne.NewSize(597, 359))
 	w.CenterOnScreen()
 	w.SetFixedSize(true)
 
 	flow := mgsmartflow.New()
 
-	lblSoftware := canvas.NewText("MiRemovePendrive - Version: "+VERSION_APP, color.Opaque)
+	lblSoftware := canvas.NewText(fmt.Sprintf("MiCheckHash - Version: %s", VERSION_APP), color.Opaque)
 	lblSoftware.TextSize = 18
 	lblSoftware.TextStyle.Bold = true
 
 	flow.AddRow(lblSoftware)
+	flow.Move(lblSoftware, 7, 7)
 
 	lblDesenvolvedor1 := widget.NewLabel("Desenvolvido por:")
 	lblDesenvolvedor1.TextStyle = fyne.TextStyle{Bold: true}
@@ -39,35 +41,50 @@ func showAbout(a fyne.App) {
 	lblSite1 := widget.NewLabel("Site:")
 	lblSite1.TextStyle = fyne.TextStyle{Bold: true}
 
-	sURL, _ := url.Parse("https://mugomes.github.io")
-	lblSite2 := widget.NewHyperlink("https://mugomes.github.io", sURL)
+	sURL, _ := url.Parse("https://www.profmugomes.com.br")
+	lblSite2 := widget.NewHyperlink("https://www.profmugomes.com.br", sURL)
 
 	flow.AddColumn(lblSite1, lblSite2)
 	flow.Resize(lblSite1, 34, 0)
 
-	lblCopyright1 := widget.NewLabel("Copyright (C) 2024-2026 Murilo Gomes Julio")
+	lblCopyright1 := widget.NewLabel("Copyright (c) 2026 Murilo Gomes Julio. All Rights Reserved.")
 	lblCopyright1.TextStyle = fyne.TextStyle{Bold: true}
 	flow.AddRow(lblCopyright1)
 
 	lblLicense1 := widget.NewLabel("License:")
 	lblLicense1.TextStyle = fyne.TextStyle{Bold: true}
 
-	lblLicense2 := widget.NewLabel("GPL-2.0-only")
+	lblLicense2 := widget.NewLabel("PolyForm Strict 1.0.0")
 
 	flow.AddColumn(lblLicense1, lblLicense2)
 	flow.Resize(lblLicense1, 62, 0)
 
 	txtLicense := widget.NewRichTextFromMarkdown(`
-	MiRemovePendrive is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, only version 2 of the License.
-	
-	MiRemovePendrive is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+This project is licensed under the PolyForm Strict License 1.0.0.
+
+### Summary
+
+This software is available for noncommercial use only.
+
+You may:
+- ✔ Use the software for noncommercial purposes.
+- ✔ Inspect and study the source code.
+
+You may not:
+- ✖ Use the software for commercial purposes.
+- ✖ Modify the software.
+- ✖ Redistribute the software.
+
+See the full license terms at [LICENSE.md](LICENSE.md).
+
+This summary is provided for convenience only.
 	`)
 	txtLicense.Wrapping = fyne.TextWrapWord
 
 	vBoxLicense := container.NewVScroll(txtLicense)
 
 	flow.AddRow(vBoxLicense)
-	flow.Resize(vBoxLicense, w.Canvas().Size().Width, 257)
+	flow.Resize(vBoxLicense, w.Canvas().Size().Width, 319)
 
 	w.SetContent(flow.Container)
 	w.Show()
